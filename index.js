e037021df566269374438e0892ab015e057231c6
Peace Ajala <peaceajala2006@gmail.com>
2025-09-20 08:08:38 +0100

    #new edit

--- index.js
+++ index.js
@@ -14,22 +14,27 @@ function solve() {
     const history = { total, amount, principle, rate, year,compound }
     result1.push(history)
     if (principle === "" || rate === "" || compound === "") {
-        display.innerHTML = "Input Your Value"
+        display.innerHTML += `<div id="display" class="alert alert-info text-center" role="alert">Input Your Value</div>`
     } else {
 
-        display.innerHTML = `The Formula for calculating it is A = P(1 + r/n)^(nt) <br>
+        display.innerHTML += `<div id="display" class="alert alert-info text-center" role="alert">The Formula for calculating it is A = P(1 + r/n)^(nt) <br>
     A= final amount<br>
     P=capital<br>
     r=rate in(%)<br>
     t=Year<br>
     n = number of times interest is compounded per year (e.g., 12 for monthly, 4 for quarterly, 1 for yearly)<br>
-    The formula for calculating compound interest is CI=A−P <br>
+    The formula for calculating compound interest is CI=A−P <br></div>
    `
-        display1.innerHTML = `
+        display1.innerHTML = `<div id="display" class="alert alert-info text-center" role="alert">
     The Final Amount (A) is :₦${amount.toFixed(2)}<br>
-    The Compound Interest is:₦${(total).toFixed(2)}`
+    The Compound Interest is:₦${(total).toFixed(2)}</div>`
 
       localStorage.setItem('compound interest', JSON.stringify(result1))
+      document.getElementById("principle").value=""
+      document.getElementById("rate").value=""
+      document.getElementById("compound").value=""
+      document.getElementById("year").value=""
+
     }
 
 
--- interest.js
+++ interest.js
@@ -11,22 +11,25 @@ function solve() {
     const his = { total, amount,principle,rate,year }
     result.push(his)
     if (principle === "" || rate === "" || year === "") {
-        display.innerHTML = "Input Your Value"
+        display.innerHTML += `<div id="display" class="alert alert-info text-center" role="alert">Input Your Value</div>`
     } else {
 
-        display.innerHTML = `The Formula for calculating it is SI = (P × R × T) / 100<br>
+        display.innerHTML += `<div id="display" class="alert alert-info text-center" role="alert">The Formula for calculating it is SI = (P × R × T) / 100<br>
     P= capital in(₦)<br>
     r= rate in(%)<br>
     t= Year<br>
-    Total= P+SI
+    Total= P+SI</div>
    `
-        display1.innerHTML = `
+        display1.innerHTML += `<div id="display" class="alert alert-info text-center" role="alert">
     The Simple Interest is :₦${amount.toFixed(2)}<br>
-    The Total is:₦${total.toFixed(2)}`
+    The Total is:₦${total.toFixed(2)}</div>`
 
 
 
         localStorage.setItem('simple interest', JSON.stringify(result))
+            document.getElementById("principle").value=""
+      document.getElementById("rate").value=""
+      document.getElementById("year").value=""
     }
 
 
